24d62e07436d0a5a2e57cc0d386cae98ee6de0b8
Zarifxon <zarifsf@mail.ru>
2022-02-08 09:38:35 +0500

    yangilandi

---
+++ phyton_6dars_vazifa.py
@@ -0,0 +1,21 @@
+# 6.1
+# Vazifa 
+# son = int(input("Sonni kiriting: "))
+# kvadrat = son**2
+# kub = son**3
+# print("Kiritgan soningizni kvadrati:", kvadrat, "Kubi esa:", kub)
+
+# 6.2
+# Vazifa 
+# yosh = int(input("Yoshingizni kititing:"))
+# t_yil = 2022 - yosh
+# print("Sizning tug'ulgan yilingiz:", t_yil, "yil!")
+# 6.3
+# Vazifa 
+a = int(input("Birinchi sonni kiriting:"))
+b = int(input("Ikkinchi sonni kiriting:"))
+yig = a+b
+ay = a-b
+kop = a*b
+bol = a/b
+print("Sonlar yig'indisi:", yig, "Ayirmasi:", ay, "Ko'paytmasi:", kop, "Bo'linmasi:", bol)
---
+++ phyton_8dars.py
@@ -0,0 +1,16 @@
+# 8- dars
+# list ro'yxatlar bilan ishlash
+#SORT TARTIB BILAN JOYLASHTIRISH alfabit bo'yicha
+cars = ['bmw', 'mersedes benz', 'volvo','general motors', 'tesla', 'audi']
+# cars.sort()
+# print(cars)
+# NAT: ['audi', 'bmw', 'general motors', 'mersedes benz', 'tesla', 'volvo']
+
+ # reverse=True ALFABITGA TESKARI TARTIBLASH 
+cars.sort(reverse=True)
+print(cars)
+# NAT: ['volvo', 'tesla', 'mersedes benz', 'general motors', 'bmw', 'audi']
+cars = ['bmw', 'mersedes benz', 'volvo','general motors', 'tesla', 'audi']
+print(sorted(cars))
+# Nat:['audi', 'bmw', 'general motors', 'mersedes benz', 'tesla', 'volvo']
+# sorted ham asl holini yo'qotmasdan alfabit bo'yicha tartiblash
